2cf9ffd3435565bc6a15f8c85bca60e55023a6cf
Ana Valdivia <anav1317@yahoo.com>
2020-04-23 11:23:24 -0700

    Added some function to calc

--- partialwba.js
+++ partialwba.js
@@ -1,5 +1,11 @@
 // console.log(wbaArray)
-var stateSelection = $(".stateselection")
+var stateSelection = $(".stateselection");
+var calculateBtn = $(".calculateBtn");
+var inputState = $("#inputState");
+var origionalWages = $("#origionalWages");
+var quarterlywages = $(".quarterlywages");
+var reducedWages = $("#reducedWages");
+var percentageReduced = $(".percentageReduced");
 
 function setUpDropDown(){
     for (let i = 0; i < wbaArray.length; i++) {
@@ -11,3 +17,20 @@ function setUpDropDown(){
 }
 
 setUpDropDown();
+
+// calculateBtn.on("click", function(e){
+//     e.preventDefault();
+//     console.log(inputState.val())
+// });
+
+$("form").on("submit", function(e){
+    e.preventDefault();
+
+    var quarterlyCalc = origionalWages.val() * 12;
+    $(quarterlywages).text(quarterlyCalc);
+
+
+    var percentRed = (reducedWages.val() / origionalWages.val()) * 100
+    $(percentageReduced).text(percentRed+"%")
+
+})
